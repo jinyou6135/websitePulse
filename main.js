e825fc2b9636021dc8f1de1ad3bbce957fbc41a0
Jin You <jinyou2009.12@gmail.com>
2026-07-07 23:26:35 -0400

    updated main.js

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <main>
-    
-  </main>
-`
 const date = document.querySelector("#datepicker").value;
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`);
