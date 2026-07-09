8a524f1a5b7eff1bd6aa1ccba439232650b8dea7
jinyou00 <jin.you@student.tdsb.on.ca>
2026-07-09 14:02:13 -0400

    added task 5

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import './style.css'
 
-const date = document.querySelector("#datepicker").value;
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`);
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+
+document.querySelector("#app").innerHTML = `<p class = "title">Loading...</p>`
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("#app").innerHTML = `<p class="title">${data.title}</p>`;
+        document.querySelector("#app").innerHTML += `<p class="copyright">${data.copyright}</p>`;
+    }
+);
+
+console.log(API_KEY);
