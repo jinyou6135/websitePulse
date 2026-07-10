f11e5184c87cd2d81683b86d1b1b5d294cfd1af0
jinyou00 <jin.you@student.tdsb.on.ca>
2026-07-09 21:13:59 -0400

    finalized all details; added date + made NASA API reload everytime the date is changed (also recorded the current date as the default)

--- src/main.js
+++ src/main.js
@@ -1,15 +1,54 @@
 import './style.css'
 
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+const datepicker = document.querySelector("#datepicker");
+
+const today = new Date();
+datepicker.value = `${today.getFullYear()}-` + `${String(today.getMonth() + 1).padStart(2, "0")}-` + `${String(today.getDate()).padStart(2, "0")}`;
+
+
+const date = document.querySelector("#datepicker").value;
 
 document.querySelector("#app").innerHTML = `<p class = "title">Loading...</p>`
 
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-        document.querySelector("#app").innerHTML = `<p class="title">${data.title}</p>`;
-        document.querySelector("#app").innerHTML += `<p class="copyright">${data.copyright}</p>`;
-    }
-);
+function loadApod(date) {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
+        .then(response => response.json())
+        .then(data => {
 
-console.log(API_KEY);
+            if (data.code) {
+                document.querySelector("#app").innerHTML = `
+                    <p class="title">Error: ${data.msg}</p>
+                `;
+                return;
+            }
+
+            let media;
+
+            if (data.media_type === "image") {
+                media = `<img src = "${data.url}" class = "media" alt = ${data.title}/>`;
+            } else if (data.url.includes("youtube")) {
+                media = `<iframe src = "${data.url}" class="media"></iframe>`;
+            } else {
+                media = `<video src = "${data.url}" controls class="media"></video>`;
+            }
+
+            document.querySelector("#app").innerHTML = `
+            <div class="background">
+                <p class="title">${data.title}</p>
+                <p class="explanation">${data.explanation}</p>
+                ${media}
+                <p class="copyright"><strong>Copyright:</strong> ${data.copyright || "NASA APOD"}</p>
+            </div>
+        `;
+    })
+    .catch(err => {
+        document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+    });
+}
+
+loadApod(datepicker.value);
+
+datepicker.addEventListener("change", () => {
+    loadApod(datepicker.value);
+});
